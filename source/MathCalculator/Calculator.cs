3fa2ae336841d5940a33a6d462873ef9578ecd25
Konstantinos Persimintzis <kpersimintzis@gmail.com>
2022-02-25 16:32:39 +0200

    Add project files.

---
+++ source/MathCalculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathCalculator
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b) => a + b;
+        public int Subtraction(int a, int b) => a - b;
+        public int Multiplication(int a, int b) => a * b;
+        public double Deviation(int a, int b) => (b!=0) ? a / b : int.MinValue;
+    }
+}
