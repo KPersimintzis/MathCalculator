1b1c6f55780edb6cbd1c476f5119b5b49ca1a148
Konstantinos Persimintzis <kpersimintzis@gmail.com>
2022-02-25 16:55:01 +0200

    Convert to static class - calculator

--- source/MathCalculator/Calculator.cs
+++ source/MathCalculator/Calculator.cs
@@ -2,11 +2,11 @@
 
 namespace MathCalculator
 {
-    public class Calculator
+    public static class Calculator
     {
-        public int Addition(int a, int b) => a + b;
-        public int Subtraction(int a, int b) => a - b;
-        public int Multiplication(int a, int b) => a * b;
-        public double Deviation(int a, int b) => (b!=0) ? a / b : int.MinValue;
+        public static int Addition(int a, int b) => a + b;
+        public static int Subtraction(int a, int b) => a - b;
+        public static int Multiplication(int a, int b) => a * b;
+        public static double Deviation(int a, int b) => (b!=0) ? a / b : int.MinValue;
     }
 }
